14d48d7266b2e05355d4c031d06e2b12d951bf1a
eman467 <aziz.eman@hotmail.com>
2026-01-22 23:32:18 -0500

    simplified workout program to exclude properties no longer needed. cleaned up commented code that wasn't being used

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import React, { useState } from "react";
 
 // CONVERT ALL PROGRAM STORAGE
-interface SetInfo {
-  reps: number;
-  weight: number;
-}
 
 interface LiftInfo {
   id: number;
   name: string;
-  volume: Array<SetInfo>;
+  sets: number;
+  reps: number;
+  weight: number;
 }
 
 interface TrainingBlockObj {
   trainingBlockWeeks: number;
   weeklyFrequency: number;
-  minimumIntensity: number;
 }
 
 interface DayDetail {
@@ -28,11 +25,6 @@ interface LiftDayDetails {
 
 interface ProgramDetails {
   week: number;
-  intensityPercent: number;
-  weeklyFrequency: number;
-  setsPerSession: number;
-  repsPerSet: number;
-  totalWeeklySetsPerLift: number;
   days: {
     [key: string]: LiftDayDetails;
   };
@@ -44,104 +36,35 @@ interface AdditionalInputs {
   };
 }
 
-const trainingBlockPercentages = (
-  trainingBlockWeeks: number,
-  weeklyFrequency: number,
-  minimumIntensity: number
-) => {
-  if (
-    trainingBlockWeeks < 1 ||
-    trainingBlockWeeks > 12 ||
-    weeklyFrequency < 1 ||
-    weeklyFrequency > 7
-  ) {
-    console.error("Values out of range: Weeks (1-12), Frequency (1-7)");
-    return -1;
-  }
-
-  const intensityVolumeAdjustments = [];
-  const minIntensity = minimumIntensity;
-  const maxIntensity = 100;
-
-  // total growth is 50%
-  const totalGrowth = maxIntensity - minIntensity;
-
-  // weekly volume increases. Take total growth which is 50% and divide it by the
-  // amount of weeks in our training block minus 1. so if the training block is 8 weeks, then
-  // 50% / 7 = 7.14% increase per week in intensity. Why do we subtract one from our training
-  // block weeks before we divide from totalGrowth? because 50% counts as the first week. so
-  // there should be 7 jumps left to get to 100% intensity.
-  const weeklyStep =
-    trainingBlockWeeks > 1 ? totalGrowth / (trainingBlockWeeks - 1) : 0;
-
-  for (let i = 0; i < trainingBlockWeeks; i++) {
-    const currentWeek = i + 1;
-    const intensity = Math.round(minIntensity + i * weeklyStep);
-
-    let repsPerSet = 0;
-    let setsPerSession = 0;
-
-    if (intensity === 100) {
-      setsPerSession = 1;
-      repsPerSet = 1;
-    } else if (intensity >= 95) {
-      setsPerSession = 3;
-      repsPerSet = 1;
-    } else if (intensity > 90) {
-      setsPerSession = 5;
-      repsPerSet = 1;
-    } else if (intensity >= 85) {
-      setsPerSession = 5;
-      repsPerSet = 2;
-    } else {
-      setsPerSession = 5;
-      repsPerSet = 5;
-    }
-
-    const totalWeeklySetsPerLift = setsPerSession * weeklyFrequency;
-
-    intensityVolumeAdjustments.push({
-      week: currentWeek,
-      intensityPercent: intensity,
-      weeklyFrequency: weeklyFrequency,
-      setsPerSession: setsPerSession,
-      repsPerSet: repsPerSet,
-      totalWeeklySetsPerLift: totalWeeklySetsPerLift,
-    });
-  }
-
-  console.table(intensityVolumeAdjustments);
-  return intensityVolumeAdjustments;
-};
-
 const mockLifts: LiftInfo[] = [
   {
     id: 1,
     name: "Back Squat",
-    volume: [{ reps: 8, weight: 225 }],
+    sets: 5,
+    reps: 5,
+    weight: 225,
   },
   {
     id: 2,
     name: "Back Squat",
-    volume: [{ reps: 8, weight: 225 }],
+    sets: 5,
+    reps: 5,
+    weight: 275,
   },
   {
     id: 3,
     name: "Back Squat",
-    volume: [{ reps: 8, weight: 225 }],
+    sets: 5,
+    reps: 5,
+    weight: 315,
   },
 ];
 
 function App() {
   const [lifts, setLifts] = useState<LiftInfo[] | null>(mockLifts);
-  const [selectedLift, setSelectedLift] = useState<LiftInfo | null>(null);
-  // const [newGoal, setNewGoal] = useState<number>(
-  //   selectedLift.personalRecord.weightInPounds
-  // );
   const [newTrainingBlock, setNewTrainingBlock] = useState<TrainingBlockObj>({
     trainingBlockWeeks: 0,
     weeklyFrequency: 0,
-    minimumIntensity: 0,
   });
   const [program, setProgram] = useState<ProgramDetails[] | null>(null);
   const [newLift, setNewLift] = useState<string>("");
@@ -152,42 +75,19 @@ function App() {
   const [additionalLiftInputs, setAdditionalLiftInputs] =
     useState<AdditionalInputs | null>(null);
 
-  // const handleGoalChange = (e: React.SyntheticEvent<HTMLInputElement>) => {
-  //   setNewGoal(Number(e.currentTarget.value));
-  // };
-
-  // const handleSelectedLift = (e: React.ChangeEvent<HTMLSelectElement>) => {
-  //   const foundLift = lifts.find((lift) => lift.name === e.currentTarget.value);
-  //   if (foundLift) {
-  //     setSelectedLift(foundLift);
-  //     setNewGoal(foundLift.personalRecord.weightInPounds);
-  //   }
-  // };
-
-  // const handleNewGoalSubmission = (
-  //   e: React.SyntheticEvent<HTMLFormElement>
-  // ) => {
-  //   e.preventDefault();
-  //   const newLiftObject: LiftInfo = {
-  //     ...selectedLift,
-  //     personalRecord: { ...selectedLift.personalRecord, currentGoal: newGoal },
-  //   };
-  //   setLifts([
-  //     ...lifts.filter((lift) => lift.name !== newLiftObject.name),
-  //     newLiftObject,
-  //   ]);
-  // };
-
   const handleTrainingProgramSubmission = (
-    e: React.SyntheticEvent<HTMLFormElement>
+    e: React.SyntheticEvent<HTMLFormElement>,
   ) => {
     e.preventDefault();
 
-    const trainingBlockInfo = trainingBlockPercentages(
-      newTrainingBlock.trainingBlockWeeks,
-      newTrainingBlock.weeklyFrequency,
-      newTrainingBlock.minimumIntensity
-    );
+    const trainingBlockInfo = [];
+
+    for (let i = 0; i < newTrainingBlock.trainingBlockWeeks; i++) {
+      trainingBlockInfo.push({
+        week: i + 1,
+        weeklyFrequency: newTrainingBlock.weeklyFrequency,
+      });
+    }
 
     const newProgram: Array<ProgramDetails> =
       expandTrainingBlockWithDays(trainingBlockInfo);
@@ -215,7 +115,6 @@ function App() {
         };
       }
     }
-    console.log("additional Lift inputs: ", newAdditionalInputsObj);
     setAdditionalLiftInputs(newAdditionalInputsObj);
   };
 
@@ -268,15 +167,9 @@ function App() {
           weeklyFrequency: inputToNumber,
         });
         break;
-      case "min-intensity-input":
-        setNewTrainingBlock({
-          ...newTrainingBlock,
-          minimumIntensity: inputToNumber,
-        });
-        break;
       default:
         console.error(
-          "something went wrong, the inputs should assigned to block duration, weekly frequency, or min intensity"
+          "something went wrong, the inputs should assigned to block duration, weekly frequency, or min intensity",
         );
     }
   };
@@ -304,7 +197,9 @@ function App() {
     const newLiftObject = {
       id: lifts ? lifts.length + 1 : 1,
       name: newLift,
-      volume: [{ reps: 8, weight: 300 }],
+      sets: 0,
+      reps: 0,
+      weight: 0,
     };
     const newLiftsArray = lifts ? [...lifts, newLiftObject] : [newLiftObject];
     setLifts(newLiftsArray);
@@ -364,7 +259,7 @@ function App() {
         }
         const dayKey = `day${j + 1}`;
         const removedUndefinedValuesLifts = dayLiftsForWeeks[i][j].filter(
-          (lift) => lift !== undefined
+          (lift) => lift !== undefined,
         );
         newProgramObject[i].days[dayKey] = {
           ...newProgramObject[i].days[dayKey],
@@ -393,34 +288,11 @@ function App() {
         [dayKey]: [...additionalLiftInputs[weekKey][dayKey], ""],
       },
     };
-    console.log("DID WE ADD ADDITIONAL INPUTS??", tempAdditionalInputObj);
     setAdditionalLiftInputs(tempAdditionalInputObj);
   };
 
   return (
     <>
-      {/* <form action="" onSubmit={handleNewGoalSubmission}>
-        <select
-          name="lifts"
-          id="lifts"
-          onChange={(e) => handleSelectedLift(e)}
-          value={selectedLift.name}
-        >
-          {lifts.map((lift) => (
-            <option key={lift.name}>{lift.name}</option>
-          ))}
-        </select>
-        {selectedLift && (
-          <input
-            value={newGoal}
-            onChange={(e) => handleGoalChange(e)}
-            type="number"
-            min={selectedLift.personalRecord.weightInPounds}
-          />
-        )}
-        <button type="submit">Add New Goal</button>
-      </form> */}
-
       <h3>Training Block Creation</h3>
       {lifts && lifts.length > 0 ? (
         lifts.map((lift) => (
@@ -470,17 +342,7 @@ function App() {
             max={7}
           />
         </div>
-        <div>
-          <label htmlFor="min-intensity-input">minimum intensity: </label>
-          <input
-            id="min-intensity-input"
-            value={newTrainingBlock.minimumIntensity}
-            onChange={(e) => handleNewTrainingBlockInputs(e)}
-            type="number"
-            min={30}
-            max={100}
-          />
-        </div>
+
         {lifts && lifts.length > 2 ? (
           <button type="submit">Create Training Block</button>
         ) : (
@@ -503,11 +365,6 @@ function App() {
       {selectedWeek && (
         <div>
           <div>Week {selectedWeek.week}</div>
-          <div>Intensity of 1RM: {selectedWeek.intensityPercent}%</div>
-          <div>Weekly Frequency: {selectedWeek.weeklyFrequency}</div>
-          <div>Sets Per Session{selectedWeek.setsPerSession}</div>
-          <div>Reps Per Set: {selectedWeek.repsPerSet}</div>
-          <div>Total Sets For Week: {selectedWeek.totalWeeklySetsPerLift}</div>
           {Object.keys(selectedWeek.days).map((day, index) => {
             return (
               <div key={day}>
